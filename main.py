7f9797c91fdcc18d623c0236872468581b5dd452
Jfrends <jfrends10@gmail.com>
2026-01-13 18:16:11 -0800

    added folder concept and paths

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ class UserCreate(BaseModel):
     username: str
     password: str
 
-class FileCreate(BaseModel):
+class FolderCreate(BaseModel):
+    path: str
     filename: str
-    folder_path: str
 
 app = FastAPI()
 port = 8000
@@ -81,7 +81,7 @@ async def list_files():
     return files
 
 @app.post("/user/{user_id}/files")
-async def user_upload_file(user_id : str, folder_path: str, file : UploadFile = File(...)):
+async def user_upload_file(user_id : str, path: str, file : UploadFile = File(...)):
     try:
         owner_id = ObjectId(user_id)
     except Exception:
@@ -91,7 +91,8 @@ async def user_upload_file(user_id : str, folder_path: str, file : UploadFile = 
     if not existing:
         raise HTTPException(status_code=404, detail="User not found")
     
-    existing = await files_collection.find_one({"folder_path": file.folder_path, 
+    existing = await files_collection.find_one({"folder_path": path,
+                                                "filename": file.filename,
                                                 "owner_id": owner_id})
     if existing:
         raise HTTPException(status_code=400, detail="File has duplicate path")
@@ -104,9 +105,75 @@ async def user_upload_file(user_id : str, folder_path: str, file : UploadFile = 
         "filename": file.filename,
         "content_type": file.content_type,
         "owner_id": owner_id,
-        "folder_path": folder_path,
+        "folder_path": path,
         "minio_key": minio_key,
+        "is_folder": False,
         "upload_timestamp": str(datetime.now(UTC))
     })
 
     return {"id": str(result.inserted_id), "filename": file.filename}
+
+@app.post("/user/{user_id}/folders")
+async def user_create_folder(user_id : str, folder: FolderCreate):
+    try:
+        owner_id = ObjectId(user_id)
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid user ID")
+    
+    existing = await users_collection.find_one({"_id": owner_id})
+    if not existing:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    existing = await files_collection.find_one({"folder_path": folder.path, 
+                                                "filename": folder.filename,
+                                                "owner_id": owner_id})
+    if existing:
+        raise HTTPException(status_code=400, detail="Folder has duplicate path")
+
+    result = await files_collection.insert_one({
+        "filename": folder.filename,
+        "owner_id": owner_id,
+        "folder_path": folder.path,
+        "is_folder": True,
+        "upload_timestamp": str(datetime.now(UTC))
+    })
+
+    return {"id": str(result.inserted_id), "filename": folder.filename}
+
+@app.delete("/files/{file_id}")
+async def delete_file(file_id : str):
+    try:
+        f_id = ObjectId(file_id)
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid file ID")
+    
+    # ADD MINIO CLEANUP 
+
+    result = await files_collection.delete_one({"_id": f_id})
+    
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="File not found")
+    
+    return {"status": "success", "deleted_id": file_id}
+
+@app.delete("/folders/{file_id}")
+async def delete_folder(file_id : str):
+    try:
+        folder_id = ObjectId(file_id)
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid file ID")
+    
+    existing = await files_collection.find_one({"_id": folder_id})
+    if not existing:
+        raise HTTPException(status_code=404, detail="Folder not found")
+    else:
+        files_inside = await files_collection.find_one({"folder_path": existing["folder_path"] + f"/{existing['filename']}/"})
+        if files_inside:
+            raise HTTPException(status_code=400, detail="Folder not empty")
+
+    result = await files_collection.delete_one({"_id": folder_id})
+    
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="File not found")
+    
+    return {"status": "success", "deleted_id": file_id}
